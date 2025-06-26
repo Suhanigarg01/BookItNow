ad4b560cd5481226d9c466e31fc59c959d64c61f
Suhani Garg <suhanigarg@Suhanis-MacBook-Air.local>
2025-06-26 17:58:41 +0530

    changed keys

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ import { stripeWebhooks } from './controllers/stripeWebhooks.js';
 const app = express();
 const port = 3000;
 
-await connectDB();
+await connectDB()
 
 // Stripe Webhooks Route
 app.use('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
@@ -24,6 +24,7 @@ app.use(express.json())
 app.use(cors())
 app.use(clerkMiddleware())
 
+
 // API Routes
 app.get('/', (req, res)=> res.send('Server is Live!'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
@@ -32,4 +33,6 @@ app.use('/api/booking', bookingRouter)
 app.use('/api/admin', adminRouter)
 app.use('/api/user', userRouter)
 
+
 app.listen(port, ()=> console.log(`Server listening at http://localhost:${port}`));
+
